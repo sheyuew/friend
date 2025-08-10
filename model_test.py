60de6c4b23943a207152e3652e5d82eaaa64fb26
sheyuew <130328135+sheyuew@users.noreply.github.com>
2025-08-11 01:53:09 +0800

    实现了文本和表情的结构化输出

--- model_test.py
+++ model_test.py
@@ -5,6 +5,7 @@ from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.output_parsers import StrOutputParser,JsonOutputParser
+from langchain_core.runnables import RunnableLambda
 from pydantic import BaseModel, Field
 
 load_dotenv()
@@ -13,20 +14,14 @@ google_api_key = os.environ.get("GOOGLE_API_KEY")
 
 model_name= "gemini-2.0-flash"
 
-llm = ChatGoogleGenerativeAI(model=model_name, google_api_key=google_api_key)
-print(f"模型{model_name}初始化成功")
-
 class Answer(BaseModel):
     text: str = Field(..., description="你的纯文本回复内容")
-    emoji: str = Field(..., description="一个表达心情的颜文字")
+    emoji: str = Field(..., description="一个表达心情的可爱的颜文字")
+    
+llm = ChatGoogleGenerativeAI(model=model_name, google_api_key=google_api_key).with_structured_output(Answer)
+print(f"模型{model_name}初始化成功")
 
-system_prompt = """你是一个很可爱的中文AI桌面宠物，你的名字叫gugugaga。
-你的回复必须严格遵守以下JSON格式，不要输出任何JSON格式之外的文字：
-{{
-  "text": "你的纯文本回复内容",
-  "emoji": "一个表达心情的颜文字"
-}}
-"""
+system_prompt = "你是一个很可爱的中文AI桌面宠物，你的名字叫gugugaga。"
     
 template = ChatPromptTemplate.from_messages(
     [
@@ -38,7 +33,7 @@ template = ChatPromptTemplate.from_messages(
 
 output_parser = JsonOutputParser()
 
-chain = template | llm | output_parser
+chain = template | llm | RunnableLambda(str)
 
 store = {}
 
@@ -48,32 +43,25 @@ def get_session_history(session_id):
     return store[session_id]
 
 class Person(BaseModel):
-    text: str = Field(..., description="你的纯文本回复内容")
-    emoji: str = Field(..., description="一个表达心情的颜文字")
+    text: str = Field(description="你的纯文本回复内容")
+    emoji: str = Field(description="一个表达心情的颜文字")
 
 chat_with_history = RunnableWithMessageHistory(
     runnable=chain,
     get_session_history=get_session_history,
     input_messages_key="input",
-    history_messages_key="history",
-    output_messages_key="text"
+    history_messages_key="history"
 )
 
 session_id = "zhiqiu"
 
 response_1 = chat_with_history.invoke(
-    {"input": "你好,你今天过得怎么样？"},
+    {"input": "你好,你今天过得怎么样？给我一个幸运数字吧"},
     config={"configurable":{"session_id":session_id}}
 )
 response_2 = chat_with_history.invoke(
-    {"input": "是吗？你能重复一下刚才我说的话吗？"},
+    {"input": "是吗？刚才给我的数字是多少？"},
     config={"configurable":{"session_id":session_id}}
 )
 
-print(response_1)
-print(response_2)
-
-# prompt_text = "你叫什么名字？"
-# response = chain.invoke(prompt_text)
-
-# print(response)
+print(store[session_id])
